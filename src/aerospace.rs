8c206ebab61d503d9949d9bd70bbc6d8782f65b9
Jonah Schüller <24317813+jonahschueller@users.noreply.github.com>
2026-04-06 09:57:14 +0200

    feat: integrate serde json deserialzation into aerospace client

--- src/aerospace.rs
+++ src/aerospace.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, de::DeserializeOwned};
 use subprocess::{Exec, Redirection};
 
 fn execute_aerospace_command(command: &str) -> Result<String, String> {
@@ -37,12 +38,24 @@ impl AerospaceCommand {
     }
 }
 
-fn query_aerospace(command: AerospaceCommand, args: Option<&str>) -> Result<String, String> {
+fn query_aerospace<T>(command: AerospaceCommand, args: Option<&str>) -> Result<T, String>
+where
+    T: DeserializeOwned,
+{
     let json_query = match args {
         Some(a) => format!("{} --json {}", command.as_str(), a),
         None => format!("{} --json", command.as_str()),
     };
-    execute_aerospace_command(&json_query)
+
+    let output = execute_aerospace_command(&json_query)?;
+
+    serde_json::from_str(&output).map_err(|error| {
+        format!(
+            "Failed to deserialize {} response: {}",
+            command.as_str(),
+            error
+        )
+    })
 }
 
 pub fn ensure_aerospace_installed() {
@@ -52,35 +65,69 @@ pub fn ensure_aerospace_installed() {
     }
 }
 
+#[derive(Debug, Deserialize)]
+struct AerospaceApp {
+    #[serde(rename = "app-bundle-id")]
+    app_bundle_id: String,
+    #[serde(rename = "app-name")]
+    app_name: String,
+    #[serde(rename = "app-pid")]
+    app_pid: i32,
+}
+
 pub fn aerospace_list_apps() {
-    let result = query_aerospace(AerospaceCommand::ListApps, None);
+    let result = query_aerospace::<Vec<AerospaceApp>>(AerospaceCommand::ListApps, None);
     match result {
-        Ok(output) => println!("{}", output),
+        Ok(output) => println!("{:#?}", output),
         Err(error) => eprintln!("Error: {}", error),
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct AerospaceWorkspace {
+    workspace: String,
 }
 
 pub fn aerospace_list_workspaces() {
-    let result = query_aerospace(AerospaceCommand::ListWorkspaces, Some("--all"));
+    let result =
+        query_aerospace::<Vec<AerospaceWorkspace>>(AerospaceCommand::ListWorkspaces, Some("--all"));
     match result {
-        Ok(output) => println!("{}", output),
+        Ok(output) => println!("{:#?}", output),
         Err(error) => eprintln!("Error: {}", error),
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct AerospaceMonitor {
+    #[serde(rename = "monitor-id")]
+    monitor_id: i32,
+    #[serde(rename = "monitor-name")]
+    monitor_name: String,
 }
 
 pub fn aerospace_list_monitors() {
-    let result = query_aerospace(AerospaceCommand::ListMonitors, None);
+    let result = query_aerospace::<Vec<AerospaceMonitor>>(AerospaceCommand::ListMonitors, None);
     match result {
-        Ok(output) => println!("{}", output),
+        Ok(output) => println!("{:#?}", output),
         Err(error) => eprintln!("Error: {}", error),
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct AerospaceWindow {
+    #[serde(rename = "window-id")]
+    window_id: i32,
+    #[serde(rename = "window-title")]
+    window_title: String,
+    #[serde(rename = "app-name")]
+    app_name: String,
 }
 
 pub fn aerospace_list_windows() {
-    let result = query_aerospace(AerospaceCommand::ListWindows, Some("--all"));
+    let result =
+        query_aerospace::<Vec<AerospaceWindow>>(AerospaceCommand::ListWindows, Some("--all"));
     match result {
-        Ok(output) => println!("{}", output),
+        Ok(output) => println!("{:#?}", output),
         Err(error) => eprintln!("Error: {}", error),
     }
 }
-
