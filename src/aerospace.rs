f426f05aff057643c93ababda6ce7112216d79ff
Jonah Schüller <24317813+jonahschueller@users.noreply.github.com>
2026-07-30 20:39:04 +0200

    refactor: improve aerospace cli impl

--- src/aerospace.rs
+++ src/aerospace.rs
@@ -1,5 +1,5 @@
 use serde::de::DeserializeOwned;
-use subprocess::{Exec, Redirection};
+use std::{fmt::format, process::Command};
 
 use serde::Deserialize;
 
@@ -38,22 +38,18 @@ pub struct AerospaceWindow {
     pub workspace: String,
 }
 
-fn execute_aerospace_command(command: &str) -> Result<String, String> {
-    const AEROSPACE_COMMAND: &str = "aerospace";
+fn execute_aerospace_command(command: &str, args: &[&str]) -> Result<String, String> {
+    let output = Command::new("aerospace")
+        .arg(command)
+        .args(args)
+        .output()
+        .map_err(|err| format!("Failed to execute aerospace CLI: {}", err))?;
 
-    let args: Vec<&str> = command.split_whitespace().collect();
-
-    let output = Exec::cmd(AEROSPACE_COMMAND)
-        .args(&args)
-        .stdout(Redirection::Pipe)
-        .stderr(Redirection::Pipe)
-        .capture()
-        .expect("Failed to execute aerospace command");
-
-    if output.success() {
-        Ok(output.stdout_str().to_string())
+    if output.status.success() {
+        String::from_utf8(output.stdout).map_err(|e| format!("Invalid UTF-8 output: {e}"))
     } else {
-        Err(output.stderr_str().to_string())
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        Err(stderr.to_string())
     }
 }
 
@@ -75,16 +71,14 @@ impl AerospaceCommand {
     }
 }
 
-fn query_aerospace<T>(command: AerospaceCommand, args: Option<&str>) -> Result<T, String>
+fn query_aerospace<T>(command: AerospaceCommand, args: &[&str]) -> Result<T, String>
 where
     T: DeserializeOwned,
 {
-    let json_query = match args {
-        Some(a) => format!("{} --json {}", command.as_str(), a),
-        None => format!("{} --json", command.as_str()),
-    };
+    let mut json_args = vec!["--json"];
+    json_args.extend_from_slice(args);
 
-    let output = execute_aerospace_command(&json_query)?;
+    let output = execute_aerospace_command(command.as_str(), &json_args)?;
 
     serde_json::from_str(&output).map_err(|error| {
         format!(
@@ -96,15 +90,14 @@ where
 }
 
 fn aerospace_output_format(included_fields: Vec<&str>) -> String {
-    included_fields
-        .iter()
-        .map(|field| format!("%{{{}}}", field))
-        .collect::<Vec<String>>()
-        .join(",")
-}
-
-fn aerospace_args(args: Vec<&str>) -> String {
-    args.join(" ")
+    format!(
+        "{}",
+        included_fields
+            .iter()
+            .map(|field| format!("%{{{}}}", field))
+            .collect::<Vec<String>>()
+            .join(" ")
+    )
 }
 
 pub struct Aerospace;
@@ -119,26 +112,32 @@ impl Aerospace {
 
     pub fn list_apps() -> Result<Vec<AerospaceApp>, String> {
         let fields = aerospace_output_format(vec!["app-bundle-id", "app-name", "app-pid"]);
-        let args = aerospace_args(vec![&fields]);
-        query_aerospace::<Vec<AerospaceApp>>(AerospaceCommand::ListApps, Some(&args))
+        query_aerospace::<Vec<AerospaceApp>>(AerospaceCommand::ListApps, &["--format", &fields])
     }
 
     pub fn list_workspaces() -> Result<Vec<AerospaceWorkspace>, String> {
         let fields = aerospace_output_format(vec!["workspace"]);
-        let args = aerospace_args(vec!["--all", &fields]);
-        query_aerospace(AerospaceCommand::ListWorkspaces, Some(&args))
+        query_aerospace(
+            AerospaceCommand::ListWorkspaces,
+            &["--all", "--format", &fields],
+        )
     }
 
     pub fn list_monitors() -> Result<Vec<AerospaceMonitor>, String> {
         let fields = aerospace_output_format(vec!["monitor-id", "monitor-name"]);
-        let args = aerospace_args(vec![&fields]);
-        query_aerospace::<Vec<AerospaceMonitor>>(AerospaceCommand::ListMonitors, Some(&args))
+        query_aerospace::<Vec<AerospaceMonitor>>(
+            AerospaceCommand::ListMonitors,
+            &["--format", &fields],
+        )
     }
 
     pub fn list_windows() -> Result<Vec<AerospaceWindow>, String> {
         let fields =
             aerospace_output_format(vec!["window-id", "window-title", "app-name", "workspace"]);
-        let args = aerospace_args(vec!["--all", &fields]);
-        query_aerospace::<Vec<AerospaceWindow>>(AerospaceCommand::ListWindows, Some(&args))
+
+        query_aerospace::<Vec<AerospaceWindow>>(
+            AerospaceCommand::ListWindows,
+            &["--all", "--format", &fields],
+        )
     }
 }
