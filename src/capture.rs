1db6db0e9def7d87a0aa2caa9eaaab15a322206b
Jonah Schüller <24317813+jonahschueller@users.noreply.github.com>
2026-07-30 21:07:19 +0200

    feat: add ability to capture the current arrangement from aerospace

---
+++ src/capture.rs
@@ -0,0 +1,35 @@
+use crate::aerospace::Aerospace;
+use crate::arrangement::{Arrangement, ArrangementWindow, ArrangementWorkspace};
+
+pub fn capture_arrangement(name: &str) -> Result<Arrangement, String> {
+    let workspaces = Aerospace::list_workspaces().unwrap();
+    let windows = Aerospace::list_windows().unwrap();
+
+    let workspaces: Vec<ArrangementWorkspace> = workspaces
+        .iter()
+        .map(|workspace| {
+            let name = workspace.workspace.to_string();
+            let windows_of_workspace = windows
+                .iter()
+                .filter(|window| window.workspace == workspace.workspace)
+                .map(|window| ArrangementWindow {
+                    app: window.app_name.clone(),
+                    title: Some(window.window_title.clone()),
+                })
+                .collect();
+
+            ArrangementWorkspace {
+                name,
+                windows: windows_of_workspace,
+            }
+        })
+        .collect();
+
+    let arrangement = Arrangement {
+        name: name.to_string(),
+        description: None,
+        workspaces: workspaces,
+    };
+
+    Ok(arrangement)
+}
